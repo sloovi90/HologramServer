3a96674714f0f2ea5b30164c8f3ebe0d4535a75a
sloovi90 <sloovi1990@gmail.com>
2018-01-26 12:14:35 +0200

    first commit
    init commit

---
+++ prizmaproj-master/Unity/Assets/AnimationManager.cs
@@ -0,0 +1,273 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+public class AnimationManager : MonoBehaviour {
+	public float repeatRateSec;
+	public string objectsTag;
+    public Camera camLeft;
+    public Camera camRight;
+    GameObject currentAnim;
+    Quaternion savedRot;
+    Vector3 savedPos;
+    Dictionary<string, Texture2D> texArr;
+    Dictionary<string, GameObject> modelDict;
+    List<GameObject> myArr;
+    public GameObject anchor;
+    public Image DebugImage;
+    public float deltaXAngle=2;
+    public float deltaYAngle = 2;
+    public float deltaZAngle = 2;
+    public float scalePercent=10;
+    int myIter=0;
+    public enum PresentationMode { Manual, Automatic }
+    public enum RotDir {None, Left, Right, Up, Down, CW, CCW };
+    public enum LightDir { None,Left,Right,Front,Back};
+    public PresentationMode mode = PresentationMode.Automatic;
+	// Use this for initialization
+	void Start () {
+
+        myArr = new List<GameObject>();
+        texArr = new Dictionary<string, Texture2D>();
+        modelDict = new Dictionary<string, GameObject>();
+        
+        foreach (GameObject o in Resources.LoadAll<GameObject>("Hologram_Repo"))
+        {
+            Debug.Log(o.name);
+            #if UNITY_EDITOR
+                Texture2D tex = UnityEditor.AssetPreview.GetAssetPreview(o);
+                byte[] bytes = tex.EncodeToPNG();
+                File.WriteAllBytes(Application.dataPath+"//Resources//" + o.name + ".png", bytes);
+#endif
+            Debug.Log(o.name);
+            byte[] image=File.ReadAllBytes("Assets/Resources/" + o.name + ".png");
+            Texture2D texture = new Texture2D(2, 2);
+            texture.LoadImage(image);
+            
+            /* Sprite curSprite = DebugImage.GetComponent<Image>().sprite;
+             DebugImage.GetComponent<Image>().sprite = Sprite.Create(texture, new Rect(0,0,tex.width,tex.height), new Vector2(0.5f,0.5f));
+             */
+            texArr.Add(o.name,texture);
+            GameObject obj = Instantiate(o) as GameObject;
+            obj.transform.Rotate(Vector3.right, 90);
+            myArr.Add(obj);
+            modelDict.Add(o.name, obj);
+        }
+        //hide all objects
+        foreach (GameObject o in myArr){
+            
+            o.transform.position = anchor.transform.position;
+            o.SetActive (false);
+        }
+
+        // flip cameras
+        camLeft.projectionMatrix = camLeft.projectionMatrix * Matrix4x4.Scale(new Vector3(1, -1, 1));
+        camRight.projectionMatrix = camRight.projectionMatrix * Matrix4x4.Scale(new Vector3(1, -1, 1));
+
+        enableNextModel();
+        InvokeRepeating("getNextModel", repeatRateSec, repeatRateSec) ;
+	}
+   
+    void enableNextModel()
+    {
+        myArr[myIter].SetActive(true);		//set next animation object to true
+        currentAnim = myArr[myIter];
+        savedPos = currentAnim.transform.position;
+        savedRot = currentAnim.transform.rotation;
+
+    }
+    void getNextModel()
+    {
+        showNextAnim(true);
+    }
+   public void showNextAnim(bool forward){
+        
+        myArr[myIter].transform.SetPositionAndRotation(savedPos, savedRot);
+
+        myArr[myIter].SetActive (false);		//set current animation object to false
+        if (forward)
+            myIter++;
+        else
+            myIter--;
+        myIter = (myIter+myArr.Count)% myArr.Count;
+        enableNextModel();
+		
+
+    }
+    public void ChangeLightIntensity(LightDir lightDir,bool intensify)
+    {
+        float factor = 1.1f;
+        if (!intensify)
+            factor = 0.9f;
+        switch (lightDir)
+        {
+            case LightDir.Left:
+                GameObject.Find("leftLight").GetComponent<Light>().intensity *=factor;
+                break;
+            case LightDir.Right:
+                GameObject.Find("rightLight").GetComponent<Light>().intensity *= factor;
+                break;
+            case LightDir.Front:
+                GameObject.Find("frontLight").GetComponent<Light>().intensity *= factor;
+                break;
+            case LightDir.Back:
+                GameObject.Find("backLight").GetComponent<Light>().intensity *= factor;
+                break;
+        }
+
+    }
+    public void ChangeSlidingSpeed(float speed)
+    {
+        repeatRateSec = speed;
+        if (mode == PresentationMode.Automatic)
+        {
+            CancelInvoke("getNextModel");
+            InvokeRepeating("getNextModel", repeatRateSec, repeatRateSec);
+        }
+    }
+    public void TogglePresentationMode()
+    {
+        if (mode == PresentationMode.Automatic)
+        {
+            CancelInvoke("getNextModel");
+            mode = PresentationMode.Manual;
+        }
+        else
+        {
+            InvokeRepeating("getNextModel", repeatRateSec, repeatRateSec);
+            mode = PresentationMode.Automatic;
+        }
+        GetComponent<MyServer>().PresentationClientNotify();
+
+    }
+    public void RotCurrentModel(RotDir rotDir)
+    {
+        Vector3 center = currentAnim.transform.position;
+        //look for one of this renderers
+        SkinnedMeshRenderer ren = currentAnim.GetComponentInChildren<SkinnedMeshRenderer>();
+        if (ren != null)
+        {
+            center = ren.bounds.center;
+        }
+        else
+        {
+            MeshRenderer render= currentAnim.GetComponentInChildren<MeshRenderer>();
+            center = render.bounds.center;
+        }
+        switch (rotDir)
+        {
+            case RotDir.CCW:
+                currentAnim.transform.RotateAround(center, currentAnim.transform.forward, -deltaZAngle);
+                break;
+            case RotDir.CW:
+                currentAnim.transform.RotateAround(center, currentAnim.transform.forward, deltaZAngle);
+                break;
+            case RotDir.Left:
+                currentAnim.transform.RotateAround(center, currentAnim.transform.up, deltaYAngle);
+                break;
+            case RotDir.Right:
+                currentAnim.transform.RotateAround(center, currentAnim.transform.up, -deltaYAngle);
+                break;
+            case RotDir.Up:
+                currentAnim.transform.RotateAround(center, currentAnim.transform.right, -deltaXAngle);
+                break;
+            case RotDir.Down:
+                currentAnim.transform.RotateAround(center, currentAnim.transform.right, deltaXAngle);
+                break;
+        }
+    }
+    public void ScaleModel(bool Up)
+    {
+        if (Up)
+            currentAnim.transform.localScale *= (100 + scalePercent) / 100.0f;
+        else
+            currentAnim.transform.localScale *= (100 - scalePercent) / 100.0f;
+    }
+    public void ClearCurrentModel()
+    {
+        currentAnim.transform.SetPositionAndRotation(savedPos, savedRot);
+    }
+    public Dictionary<string,Texture2D> GetImages()
+    {
+        return texArr;
+    }
+    public void ShowModel(string name)
+    {
+        if (mode == PresentationMode.Automatic)
+        {
+            CancelInvoke("getNextModel");
+            InvokeRepeating("getNextModel", repeatRateSec, repeatRateSec);
+        }
+        myArr[myIter].transform.SetPositionAndRotation(savedPos, savedRot);
+
+        myArr[myIter].SetActive(false);
+        GameObject obj = modelDict[name];
+        
+        for(int i=0;i<myArr.Count;i++)
+            if (obj == myArr[i]) {
+                myIter = i;
+                break;
+            }
+        enableNextModel();
+       
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.Q))
+        {
+
+            RotCurrentModel(RotDir.CCW);
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            RotCurrentModel(RotDir.CW);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            RotCurrentModel(RotDir.Up);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            RotCurrentModel(RotDir.Down);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            RotCurrentModel(RotDir.Right);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            RotCurrentModel(RotDir.Left);
+        }
+        else if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            if (mode == PresentationMode.Manual)
+                showNextAnim(true);
+
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            if (mode == PresentationMode.Manual)
+                showNextAnim(false);
+        }
+        else if (Input.GetKeyUp(KeyCode.P))
+        {
+            TogglePresentationMode();
+        }
+        else if (Input.GetKeyUp(KeyCode.C))
+        {
+            ClearCurrentModel();
+        }else if (Input.GetKey(KeyCode.Plus) || Input.GetKey(KeyCode.KeypadPlus))
+        {
+            ScaleModel(true);
+        }else if (Input.GetKey(KeyCode.Minus) || Input.GetKey(KeyCode.KeypadMinus))
+        {
+            ScaleModel(false);
+        }
+    }
+}
---
+++ prizmaproj-master/Unity/Assets/MyServer.cs
@@ -0,0 +1,203 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Text;
+using System;
+public class MyServer : MonoBehaviour
+{
+    int connectionId;
+    int maxConnections = 10;
+    int reliableChannelID;
+    int hostId;
+    int port= 8888;
+    byte error;
+    public AnimationManager manager;
+    int CurrentConID = -1;
+    // Use this for initialization
+    void Start()    
+    {
+        /*server = new NetworkServerSimple();
+        if(server.Listen("127.0.0.1", 5656))
+            GameObject.Find("DebugText").GetComponent<TextMesh>().text = "waiting for connection";
+        server.RegisterHandler(MsgType.Connect, OnConnected);*/
+        NetworkTransport.Init();
+        ConnectionConfig config = new ConnectionConfig();
+        reliableChannelID = config.AddChannel(QosType.ReliableSequenced);
+        HostTopology topology = new HostTopology(config, maxConnections);
+        hostId = NetworkTransport.AddHost(topology, port,null);
+        Debug.Log("Socket Open! Host ID is " + hostId);
+    }
+    AnimationManager.RotDir ConvertToRotDir(string dir)
+    {
+        switch (dir)
+        {
+            case "Left":
+                return AnimationManager.RotDir.Left;
+            case "Right":
+                return AnimationManager.RotDir.Right;
+            case "Up":
+                return AnimationManager.RotDir.Up;
+            case "Down":
+                return AnimationManager.RotDir.Down;
+            case "Cw":
+                return AnimationManager.RotDir.CW;
+            case "Ccw":
+                return AnimationManager.RotDir.CCW;
+            default:
+                return AnimationManager.RotDir.None;
+        }
+
+    }
+    AnimationManager.LightDir ConvertToLightDir(string dir)
+    {
+        switch (dir)
+        {
+            case "Left":
+                return AnimationManager.LightDir.Left;
+            case "Right":
+                return AnimationManager.LightDir.Right;
+            case "Front":
+                return AnimationManager.LightDir.Front;
+            case "Back":
+                return AnimationManager.LightDir.Back;
+         
+            default:
+                return AnimationManager.LightDir.None;
+        }
+
+    }
+    void HandleMsg(string msg)
+    {
+        string[] message = msg.Split('|');
+        switch (message[0])
+        {
+            case "P":
+                manager.TogglePresentationMode();
+                break;
+            case "SlidingSpeed":
+                manager.ChangeSlidingSpeed(float.Parse(message[1]));
+                break;
+            case "AxisRotation":
+                if (message[1] == "X")  manager.deltaXAngle = int.Parse(message[2]);
+                if (message[1] == "Y") manager.deltaYAngle = int.Parse(message[2]);
+                if (message[1] == "Z") manager.deltaZAngle = int.Parse(message[2]);
+                break;
+            case "Scale":
+                if (message[1] == "Up") manager.ScaleModel(true);
+                if (message[1] == "Down") manager.ScaleModel(false);
+                break;
+            case "Rotate":
+                manager.RotCurrentModel(ConvertToRotDir(message[1]));
+                break;
+            case "Light":
+                bool intensify = false;
+                if (message[2] == "Lighten")
+                    intensify = true;
+                manager.ChangeLightIntensity(ConvertToLightDir(message[1]), intensify);
+                break;
+            case "Clear":
+                manager.ClearCurrentModel();
+                break;
+            case "NextModel":
+                manager.showNextAnim(true);
+                break;
+            case "PrevModel":
+                manager.showNextAnim(false);
+                break;
+            case "ModelRequest":
+                Debug.Log(name + " was requested");
+                manager.ShowModel(message[1]);
+                break;
+        }
+    }
+     void Update()
+    {
+        int recHostID;
+        int recConnectionID;
+        int recChannelID;
+        byte[] buffer = new byte[1024];
+        int bufferSize = 1024;
+        int dataSize;
+        NetworkEventType recNetworkEvent = NetworkTransport.Receive(out recHostID, out recConnectionID, out recChannelID, buffer, bufferSize, out dataSize, out error);
+        
+        switch (recNetworkEvent)
+        {
+            case NetworkEventType.DataEvent:
+                string msg = Encoding.Unicode.GetString(buffer,0,dataSize);
+                //GameObject.Find("DebugText").GetComponent<TextMesh>().text = msg;
+                Debug.Log("Received " + msg+"From conId: "+recConnectionID);
+                HandleMsg(msg);
+                SendMsg("server accepted", recConnectionID);
+                Debug.Log("sent message");
+                break;
+            case NetworkEventType.ConnectEvent:
+                Debug.Log("some1 with conID " + recConnectionID + " connected!");
+                CurrentConID = recConnectionID;
+                SendMsg("server connected", recConnectionID);
+                SendInitData(recConnectionID);
+                break;
+            case NetworkEventType.DisconnectEvent:
+                Debug.Log("some1 with conID " + recConnectionID + " disconnected!");
+                CurrentConID = -1;
+                break;
+            default:
+                break;
+        }
+    }
+    public void SendMsg(string msg,int conId)
+    {
+            byte[] buffer = Encoding.Unicode.GetBytes(msg);
+            NetworkTransport.Send(hostId, conId, reliableChannelID, buffer, sizeof(char) * msg.Length, out error);
+        
+    }
+    public void SendBytes(byte[] data,int conId)
+    {
+        Debug.Log(data.Length);
+        NetworkTransport.Send(hostId, conId, reliableChannelID, data, data.Length, out error);
+    }
+    void SendInitData(int conID)
+    {
+        SendMsg("InitStart",conID);
+        SendMsg("Presentation|" + (manager.mode == AnimationManager.PresentationMode.Automatic ? "auto" : "manual"),conID);
+        SendMsg("AxisRotation|X|" + manager.deltaXAngle,conID);
+        SendMsg("AxisRotation|Y|" + manager.deltaYAngle, conID);
+        SendMsg("AxisRotation|Z|" + manager.deltaZAngle, conID);
+        SendMsg("SlidingSpeed|" + manager.repeatRateSec, conID);
+        Dictionary<string, Texture2D> images = manager.GetImages();
+        SendMsg("SendingImages|" + images.Count,conID);
+        foreach (string imageName in images.Keys)
+        {
+            SendImage(images[imageName], conID, imageName);
+        }
+        SendMsg("FinishInit", conID);
+    }
+    void SendImage(Texture2D tex,int conID,string imageName)
+    {
+        byte[] data = tex.EncodeToPNG();
+        SendMsg("ExpectImage|" + imageName + "|" + data.Length, conID);
+        //send chunks of 1024 byte
+        int chunkAmmount = (data.Length / 1024);
+        for (int chunks = 0; chunks < chunkAmmount; ++chunks)
+        {
+            byte[] chunk = new byte[1024];
+            for (int i = 0; i < 1024; i++)
+                chunk[i] = data[chunks * 1024 + i];
+            SendBytes(chunk, conID);
+        }
+        //send the remaining if it wasn't a multiply of 1024
+        int remainingBytes = data.Length - chunkAmmount * 1024;
+        byte[] remainChunk = new byte[remainingBytes];
+        for (int i = 0; i < remainingBytes; i++)
+        {
+            remainChunk[i] = data[chunkAmmount * 1024 + i];
+        }
+        SendBytes(remainChunk, conID);
+    }
+    public void PresentationClientNotify()
+    {
+        SendMsg("Presentation|" + (manager.mode == AnimationManager.PresentationMode.Automatic ? "auto" : "manual"), CurrentConID);
+    }
+    // Update is called once per frame
+
+}
